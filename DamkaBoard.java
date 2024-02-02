71497e2a9908e7f680a943c70a8c8cbfe89e32d2
tal katz <tal.katz@runi.ac.il>
2024-02-02 20:40:30 +0200

    remove space for divisiors

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,17 +2,18 @@ public class DamkaBoard {
 
 	public static void main(String[] args) {
 	int t = Integer.parseInt(args[0]);
-	for (int i=0; i<t; i++)
+	for (int i = 0; i < t; i++)
 	{
-		// System.out.print("* ");
-		for (int a = 0; a < t ;a++)
+		for (int a =0; a < t ;a++)
 		{
 			System.out.print("* ");
 		}
 		System.out.println(" ");
 		if(i%2 == 0){
-			 System.out.print(" *");
+			 System.out.print(" ");
 		}
 	}
 		}
 	}
+
+
