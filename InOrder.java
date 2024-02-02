a04c88904191f97286997a330f5112694fabbd28
tal katz <tal.katz@runi.ac.il>
2024-02-02 21:25:27 +0200

    fix in order

--- InOrder.java
+++ InOrder.java
@@ -4,17 +4,15 @@ public class InOrder {
 	   
 	  int x0 = (int)(10 + Math.random()* (-10));
 	  int x1 = (int)(10 + Math.random()* (-10)); 
-   
-   
-			 
-		  
+	  
+	  System.out.print( x0 + " " ); 
 		  while (x1>x0) {
-			System.out.print( x1 + " "); 
+			System.out.print( x1+ " " ); 
 			x0 = x1 ;
 			 x1 = (int)(10 + Math.random()* (-10)); 
 		  
 		  }
-		 System.out.println();
+	
 	}  
    }
    
